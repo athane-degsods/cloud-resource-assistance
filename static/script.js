c7b489f7b48d25bdffb7f7c222d9e935adf6b126
athane-degsods <emaildelamviec27422@gmail.com>
2026-07-31 15:43:22 -0700

    Fix bug on the frontend and missing wrapper in app.py

--- static/script.js
+++ static/script.js
@@ -3,23 +3,38 @@ const message = document.getElementById("message");
 const log = document.getElementById("log");
 
 sendBtn.addEventListener("click", () => {
-
     const userMessage = message.value;
 
     log.innerHTML += "<p><strong>You:</strong> " + userMessage + "</p>";
 
-    fetch("http://127.0.0.1:5000/chat", {
+    fetch("/chat", {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            message: userMessage
-        })
+            message: userMessage,
+        }),
     })
-    .then(response => response.json())
-    .then(data => {
-        log.innerHTML += "<p><strong>AI:</strong> " + data.readable_response + "</p>";
-    });
-
+        .then(async (response) => {
+            const data = await response.json().catch(() => null);
+            if (!response.ok) {
+                const err =
+                    (data && (data.error || data.summary)) ||
+                    "HTTP " + response.status;
+                throw new Error(err);
+            }
+            return data;
+        })
+        .then((data) => {
+            log.innerHTML +=
+                "<p><strong>AI:</strong> " +
+                (data.readable_response || JSON.stringify(data)) +
+                "</p>";
+        })
+        .catch((err) => {
+            log.innerHTML +=
+                "<p><strong>Error:</strong> " + err.message + "</p>";
+            console.error(err);
+        });
 });
